8ec9b56aaceb8a794d4043f97154625f67064797
Jianhang Chen <cjhastro@gmail.com>
2022-01-21 00:09:52 +0100

    add vis_jekkniff

--- casa/casa_utils.py
+++ casa/casa_utils.py
@@ -3,16 +3,29 @@
 """
 import os
 import re
-import json
 import numpy as np
-from astropy.coordinates import SkyCoord
 
-try:
-    # for casa6
-    from casatool import table as tbtool
-except:
-    from taskinit import tbtool
+def vis_jackknif(vis, copy=False, outdir=None):
+    """make jackknifed image with only noise"""
+    if copy:
+        if outdir is None:
+            raise ValueError('outdir must be given to do the copy!')
+        if not os.path.isdir(outdir):
+            os.system('mkdir {}'.format(outdir))
+        vis_copied = os.path.join(outdir, os.path.basename(vis) + '.copy')
+        os.system('cp -r {} {}'.format(vis, vis_copied))
+        vis = vis_copied
+    tb.open(vis, nomodify=False)
 
+    for row in range(0, tb.nrows(), 2):
+        key = 'r'+str(row+1)
+        row_data = tb.getvarcol('DATA', row, 1)[key]
+        row_data_inverse = (-1.+0j) * row_data
+        #tb.removecorowkey)
+        tb.putvarcol('DATA', {key:row_data_inverse}, row, 1)
+        tb.flush()
+    tb.close()
+    return vis
 
 def read_spw(vis):
     """read the spectral windows
@@ -20,10 +33,10 @@ def read_spw(vis):
     tb = tbtool()
     if isinstance(vis, str):
         vis = [vis, ]
-    
+
     if not isinstance(vis, list):
         raise ValueError("read_spw: Unsupported measurements files!")
-    
+
     spw_specrange = {}
 
     for v in vis:
@@ -35,20 +48,8 @@ def read_spw(vis):
         for key in col_names.keys():
             freq_max = np.max(col_freq[key]) / 1e9
             freq_min = np.min(col_freq[key]) / 1e9
-            spw_specrange[key] = [freq_min, freq_max]
+            freq_interv = np.abs(np.mean(np.diff(col_freq[key], axis=0))) / 1e9
+            freq_nchan = len(col_freq[key])
+            spw_specrange[key] = [freq_min, freq_max, freq_interv, freq_nchan]
 
-    return spw_specrange.values()
-
-def read_refdir(vis):
-    """read the reference direction and return the standard direction string
-    """
-    tb = tbtool()
-    tb.open(vis+'/FIELD')
-    reference_dir = tb.getcol('REFERENCE_DIR').flatten()
-    tb.close()
-    
-    rad2deg = 180./np.pi
-    direction = "J2000 " + SkyCoord(reference_dir[0]*rad2deg, reference_dir[1]*rad2deg, 
-                                      unit="deg").to_string('hmsdms')
-    return direction
-
+    return list(spw_specrange.values())
